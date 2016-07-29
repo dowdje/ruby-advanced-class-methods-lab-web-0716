84cd1df25f0cb405ef6f4b23cf2034341043d65c
Jacob Dowd <dowdje89@gmail.com>
2016-07-28 20:23:41 -0400

    Finished Silly Song Lab-Advanced Class Methods

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,68 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song.save
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.new
+    song.name = title
+    song.save
+    song
+  end
+
+  def self.find_by_name(title)
+    self.all.detect{|song| song.name == title}
+  end
+
+  def self.find_or_create_by_name(title)
+  
+      if self.find_by_name(title)
+        self.find_by_name(title)
+      else
+        create_by_name(title)
+      end              
+  end 
+
+  def self.alphabetical
+    self.all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(artist_song)
+    song_array = artist_song.split(" - ")
+    song = self.new
+    song.artist_name = song_array[0]
+    song.name = song_array[1].split(".")[0]
+    song
+  end
+
+
+  def self.create_from_filename(artist_song)
+    song_array = artist_song.split(" - ")
+    song = self.new
+    song.artist_name = song_array[0]
+    song.name = song_array[1].split(".")[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
+
+
+
+
 end
